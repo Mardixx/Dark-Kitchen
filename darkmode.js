462e2bae49669986ad70a0ecd2c10f49b916de92
iradukunda okly <iradukundaokly@gmail.com>
2023-09-27 10:27:55 +0200

    change background mode dark

--- darkmode.js
+++ darkmode.js
@@ -7,6 +7,8 @@ const enableDarkMode = () => {
     document.body.classList.add("darkmode");
     //update darkmode in the local storage
     localStorage.setItem("darkMode", "enabled");
+     // Add a black background to the darkmode-toggle button
+     darkModeToggle.style.backgroundColor = "black";
 }
 
 //disable dark mode
@@ -15,15 +17,17 @@ const disableDarkMode = () => {
     document.body.classList.remove("darkmode");
     //update darkmode in the local storage
     localStorage.setItem("darkMode", null);
+    // Add a white background to the darkmode-toggle button
+    darkModeToggle.style.backgroundColor = "white";
 }
 
 //stay in dark mode or not when refreshed
 if(darkMode === "enabled"){
     enableDarkMode();
-    darkModeToggle.innerHTML = '<img src="./ressources/icons/sun.svg" alt="Dark disable">'
+    darkModeToggle.innerHTML = '<img src="./ressources/icons/moon.svg" alt="Dark disable">'
 }
 else{
-    darkModeToggle.innerHTML = '<img src="./ressources/icons/moon.svg" alt="Dark enable">'
+    darkModeToggle.innerHTML = '<img src="./ressources/icons/sun.svg" alt="Dark enable">'
 }
 
 
